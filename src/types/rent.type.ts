5eb97da9c42999f0ebf191e0b9d73134c7bfb1f1
Эрнест <101067653+Nexoner@users.noreply.github.com>
2023-05-17 16:56:07 +0300

    tsv-reader setup

---
+++ src/types/rent.type.ts
@@ -0,0 +1,21 @@
+import { Images } from "./rent-images.type";
+
+export type Rent = {
+    title: string;
+    description: string;
+    postDate: Date;
+    city: string;
+    mainImage: string;
+    images: Images[];
+    premium: string;
+    favorite: string;
+    rate: number;
+    type: string;
+    roomsNumber: number;
+    guestNumber: number;
+    price: number;
+    conveniences: string;
+    author: string;
+    commentCount: number;
+    cords: number;
+}
